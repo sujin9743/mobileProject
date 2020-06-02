284ec064262fb0a1efe15f4dac9b97fc300ba2db
수진 <sujin9743@users.noreply.github.com>
2020-06-02 19:31:38 +0900

    mobileProject

---
+++ app/src/main/java/jsj/mjc/mobileproject/MainActivity.java
@@ -0,0 +1,14 @@
+package jsj.mjc.mobileproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
